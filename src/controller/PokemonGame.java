e21ff7d0873b4a076beb270e1b67eb28b62af9a4
Daniel Lopez <daniellopez@email.arizona.edu>
2018-04-29 13:40:42 -0700

    Okay branch

--- src/controller/PokemonGame.java
+++ src/controller/PokemonGame.java
@@ -471,7 +471,6 @@ public class PokemonGame extends Application {
   		gameBackground.movePlayer(event.getCode(), "under");
   	}
   }
-  
 
   /*
    * getWildPoke() -- after checkBush() returns true (it found a Pokemon), this
